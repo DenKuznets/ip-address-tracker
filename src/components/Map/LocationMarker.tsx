6c3b318f3581ababaf7312986ad78dca68b397ed
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-08-08 09:50:22 +0300

    map rerenders on new location

--- src/components/Map/LocationMarker.tsx
+++ src/components/Map/LocationMarker.tsx
@@ -1,18 +1,21 @@
-import React, { useEffect } from "react";
-import { Marker, Popup } from "react-leaflet";
+import React, { useEffect, useState } from "react";
+import { Marker, Popup, useMapEvents } from "react-leaflet";
+import { useAppStore } from "../../AppStore";
 
 function LocationMarker() {
     const [position, setPosition] = useState(null);
+    
+    const latlng = useAppStore((state) => state.latlng);
 
     const map = useMapEvents({
         locationfound(e) {
-            // console.log(e);
+            console.log(e);
             setPosition(e.latlng);
             map.flyTo(e.latlng, map.getZoom());
         },
     });
-
-   
+    
+    map.setView(latlng);
 
     return position === null ? null : (
         <Marker position={position}>
